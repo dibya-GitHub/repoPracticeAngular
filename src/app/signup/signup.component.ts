e2a5ba347d0a43f0036a1273153cb13671be2325
Dibya ranjan <sahu.dibya93@gmail.com>
2019-03-20 22:16:02 +0530

    commiting next functionallity

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -7,24 +7,32 @@ import { FormGroup, FormControl, FormBuilder,Validators } from '@angular/forms';
 })
 export class SignupComponent implements OnInit {
   profileForm: FormGroup;
-
+  submitted:boolean = false;
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.profileForm = this.fb.group({
-      firstName: ['', Validators.required], 
-      lastName: ['', Validators.required],
-      email: ['', Validators.required,Validators.email],
-      password: ['', Validators.required],
-      re_password: ['', Validators.required],
-      birthDate: ['', Validators.required],
-      phoneNumber: ['', Validators.required,Validators.maxLength(10)],
+      firstName: ['', Validators.compose([Validators.required])],
+      lastName: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required,Validators.email])],
+      password: ['', Validators.compose([Validators.required,Validators.minLength(6)])],
+      re_password: ['', Validators.compose([Validators.required])],
+      birthDate: ['', Validators.compose([Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required,Validators.maxLength(10)])]
     });
   }
-
+  get form(){
+    return this.profileForm.controls;
+  }
 
   onSubmit() {
+    this.submitted = true;
+    // if(this.profileForm.invalid){
+    //   return true;
+    // } 
     // TODO: Use EventEmitter with form value
     console.log(this.profileForm.value);
-  }
+    console.log(this.profileForm.controls);
+  } 
 }
